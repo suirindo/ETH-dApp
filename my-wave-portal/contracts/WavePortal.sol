262484a4515fcc6b8d346f7eb3ada56657b43eec
asagiman <lagavulin.jb.dc@gmail.com>
2022-04-09 18:25:19 +0900

    1

--- my-wave-portal/contracts/WavePortal.sol
+++ my-wave-portal/contracts/WavePortal.sol
@@ -1,20 +1,55 @@
 // SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.4;
-
 import "hardhat/console.sol";
 
 contract WavePortal {
-
     uint256 totalWaves;
+    // NewWaveイベントの作成
+    event NewWave(address indexed from, uint256 timestamp, string message);
+    // waveという構造体を作成。
+    // 構造体の中身は、カスタマイズすることができる
 
-    constructor() {
-        console.log("Here is my first smart contract!");
+    struct Wave {
+        address waver; //waveを送ったユーザーのアドレス
+        string message; //ユーザーが送ったメッセージ
+        uint256 timestamp; //ユーザーがwaveを送った瞬間のタイムスタンプ
     }
 
-    function wave() public {
+    // 構造体の配列を格納するための変数wavesを宣言
+    // これで、ユーザーが送ってきたすべてのwaveを保持することができる
+    Wave[] waves;
+    constructor() payable {
+        console.log("We have been constructed!");
+    }
+
+    // _messageという文字列を要求するようにwave関数を更新
+    // _messageは、ユーザーがフロントエンドから送信するメッセージ
+
+    function wave(string memory _message) public {
         totalWaves += 1;
-        console.log("%s has waved!", msg.sender);
+        console.log("%s waved w/ message %s", msg.sender, _message);
+
+        //waveとメッセージを配列に格納
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        // コントラクト側でemitされたイベントに関する通知をフロントエンドで取得できるようにする
+        emit NewWave(msg.sender, block.timestamp, _message);
+
+        // waveを送ってくれたユーザーに0.0001ETHを送る
+        uint256 prizeAmount = 0.0001 ether;
+        require(
+            prizeAmount <= address(this).balance,
+            "Trying to withdraw more money than the contract has."
+        );
+        (bool success, ) = (msg.sender).call{value: prizeAmount}(" ");
+        require(success, "Failed to withdraw money from contract.");
+    }
+
+    // 構造体配列のwavesを返してくれるgetAllWavesという関数を追加
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
--- my-wave-portal/scripts/run.js
+++ my-wave-portal/scripts/run.js
@@ -2,28 +2,37 @@
 
 const main = async () => {
     // WavePortalコントラクトがコンパイルされ、コンパイルを扱うために必要なファイルがartifatcsディレクトリ直下に生成される。
-    const [owner, randomPerson1, randomPerson2] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
-    const wavePortal = await waveContract.deployed();
+    // デプロイする際に0.1ETHをコントラクトに提供する
+    const waveContract = await waveContractFactory.deploy({
+        value: hre.ethers.utils.parseEther("0.1"),
+    });
+    await waveContract.deployed();
+    console.log("Contract deployed to: ", waveContract.address);
 
-    console.log("Contract deployed to: ", wavePortal.address);
-    console.log("Contract deployed by: ", owner.address);
+    // コントラクトの残高を取得（0.1ETH）であることを確認
+    let contractBalance = await hre.ethers.provider.getBalance(
+        waveContract.address
+    );
+    console.log(
+        "Contract balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
+    
+    // waveを取得
+    let waveTxn = await waveContract.wave("A message!");
+    await waveTxn.wait(); // トランザクションが承認されるのを待つ
 
-    let waveCount;
-    waveCount = await waveContract.getTotalWaves();
-
-    let waveTxn = await waveContract.wave();
-    await waveTxn.wait();
-
-    waveCount = await waveContract.getTotalWaves();
-
-    waveTxn = await waveContract.connect(randomPerson1, randomPerson2).wave();
-    await waveTxn.wait();
-
-    waveCount = await waveContract.getTotalWaves();
+    // コントラクトの残高を取得し、Waveを取得した後の結果を出力
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    // コントラクトの残高から0.0001ETH引かれていることを確認
+    console.log(
+        "Contract balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
+    let allWaves = await waveContract.getAllWaves();
+    console.log(allWaves);
 };
-
 const runMain = async () => {
     try {
         await main();
